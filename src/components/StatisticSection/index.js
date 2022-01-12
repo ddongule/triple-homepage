46c3eec71f89e0a2667951fff7cf614a79faf720
Min Kyung Kang <mnk918p@gmail.com>
2022-01-12 23:48:49 +0900

    refactor: 들어간 데이터 분리

--- src/components/StatisticSection/index.js
+++ src/components/StatisticSection/index.js
@@ -1,39 +1,33 @@
 import React from 'react'
 
 import './index.scss'
-import triple3x from '../../assets/images/triple@2x.png'
-import appStore from '../../assets/images/app-store@2x.png'
-import playStore from '../../assets/images/play-store@2x.png'
+
 import ImageWithInfo from '../ImageWithInfo'
 import InfoText from '../InfoText'
+import { statisticInfos } from '../../data/statics'
 
 const StatisticSection = () => {
+  const { contentMain, awardDetails, details } = statisticInfos
+
   return (
     <section className="statistics-wrapper">
       <div className="content-logo">
-        <ImageWithInfo
-          imageSrc={triple3x}
-          infoText="2019년 2월 기준"
-          align="col"
-        />
+        {contentMain.map(([img, text], idx) => (
+          <ImageWithInfo imageSrc={img} infoText={text} align="col" key={idx} />
+        ))}
       </div>
 
       <div className="awards-details">
         <div className="info">
-          <InfoText emphasisText="350만 명" contents="의 사용자" />
-          <InfoText emphasisText="21만 개" contents="의 여행 리뷰" />
-          <InfoText emphasisText="650만 개" contents="의 여행 일정" />
+          {awardDetails.map(([emphasis, rest], idx) => (
+            <InfoText emphasisText={emphasis} contents={rest} key={idx} />
+          ))}
         </div>
 
         <div className="details">
-          <ImageWithInfo
-            imageSrc={playStore}
-            infoText={`2018 구글 플레이스토어 \n 올해의 앱 최우수상 수상`}
-          />
-          <ImageWithInfo
-            imageSrc={appStore}
-            infoText={`2018 애플 앱스토어 \n 오늘의 여행앱 선정`}
-          />
+          {details.map(([img, text], idx) => (
+            <ImageWithInfo imageSrc={img} infoText={text} key={idx} />
+          ))}
         </div>
       </div>
     </section>
---
+++ src/data/statics.js
@@ -0,0 +1,16 @@
+import triple3x from '../assets/images/triple@2x.png'
+import appStore from '../assets/images/app-store@2x.png'
+import playStore from '../assets/images/play-store@2x.png'
+
+export const statisticInfos = {
+  contentMain: [[triple3x, `2019년 2월 기준`]],
+  awardDetails: [
+    ['350만 명', '의 사용자'],
+    ['21만 개', '의 여행 리뷰'],
+    ['650만 개', '의 여행 일정'],
+  ],
+  details: [
+    [playStore, `2018 구글 플레이스토어 \n 올해의 앱 최우수상 수상`],
+    [appStore, `2018 애플 앱스토어 \n 오늘의 여행앱 선정`],
+  ],
+}
